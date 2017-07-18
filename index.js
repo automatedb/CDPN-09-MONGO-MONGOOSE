ce2409045e0e97c184c0198a24eaa5e2d4fbe452
automatedb <contact@robots-trading.com>
2017-07-18 14:39:19 +0200

    Initial commit

--- index.js
+++ index.js
@@ -9,26 +9,33 @@ mongoose.createConnection('mongodb://localhost:27017/data', {
 }).then(
     db => {
         console.log("Connected on 27017");
+        
         // DEMARRAGE DE L'APPLICATION
 
         const users = db.model('Users');
 
-        return users.create([
-            {
-                email: 'john.doe@domain.tld',
-                firstname: 'John',
-                lastname: 'Doe',
-                password: 'password'
-            },
-            {
-                email: 'jane.doe@domain.tld',
-                firstname: 'John',
-                lastname: 'Doe',
-                password: 'password'
-            }
-        ]).then(
-            result => console.log(result)
-        ).catch(e => console.log(e));
+        // Création de données
+        // users.create([
+        //     {
+        //         email: 'john.doe@domain.tld',
+        //         firstname: 'John',
+        //         lastname: 'Doe',
+        //         password: 'password'
+        //     },
+        //     {
+        //         email: 'jane.doe@domain.tld',
+        //         firstname: 'John',
+        //         lastname: 'Doe',
+        //         password: 'password'
+        //     }
+        // ]).then(
+        //     result => console.log(result)
+        // ).catch(e => console.log(e));
+
+        // Récupération de données
+        // users.find({email: 'jane.doe@domain.tld'}, { password: 0 }).then(
+        //     results => console.log(results)
+        // ).catch(e => console.log(e));
     }
 ).catch(e => console.log(e));
 
